7487b8b1459f85ea498b32c75d7d3b7fb4ff62b6
arpit <223622257+iamarpitzala@users.noreply.github.com>
2025-12-25 17:57:36 +0530

    chore: increased jwt expiry time

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -18,7 +18,7 @@ func GenerateToken(userID int, email, secret string) (string, error) {
 		UserID: userID,
 		Email:  email,
 		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(30 * 24 * time.Hour)),
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 			NotBefore: jwt.NewNumericDate(time.Now()),
 		},
